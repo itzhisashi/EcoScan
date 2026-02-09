81877365b3be8d69948be1fe4263635f34b7eb0a
Hisashi 久 <80805078+itzhisashi@users.noreply.github.com>
2026-02-09 17:28:11 +0530

    Update dashboard.js

--- netlify/functions/dashboard.js
+++ netlify/functions/dashboard.js
@@ -1,4 +1,4 @@
-const API_URL = "PASTE_YOUR_GOOGLE_SCRIPT_URL";
+const API_URL = "https://script.google.com/macros/s/AKfycbzN7fRDD8UYY0sCtyn774gBo5kEgmGHKoGVomYwxQlhBymVeSORZB12FAjhiGzkxPlyOA/exec";
 
 async function getIP() {
   const res = await fetch("https://api.ipify.org?format=json");
