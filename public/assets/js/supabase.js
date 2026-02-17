82d6e0988625be8def3b7a17fee06a8636463247
Hisashi 久 <80805078+itzhisashi@users.noreply.github.com>
2026-02-17 14:15:00 +0530

    Delete public/assets/js/supabase.js

--- public/assets/js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY
-);
